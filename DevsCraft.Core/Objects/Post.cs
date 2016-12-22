9c6e149fcf81221bd001b91647bff4fed077857c
Moinul Islam <moinulmithu@hotmail.com>
2016-12-22 23:45:27 +0600

    Post Added

---
+++ DevsCraft.Core/Objects/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevsCraft.Core.Objects
+{
+    class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public string UrlSlug { get; set; }
+        public string Published { get; set; }
+        public DateTime PostedOn { get; set; }
+        public DateTime Modified { get; set; }
+        public string Category { get; set; }
+    }
+}
