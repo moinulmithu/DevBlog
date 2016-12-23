f58f173980131cef0ca6a7ac66278ffa1fed3b1b
Moinul Islam <moinulmithu@hotmail.com>
2016-12-24 01:00:08 +0600

    IRepo and Repo added

---
+++ DevsCraft.Core/BlogRepository.cs
@@ -0,0 +1,33 @@
+﻿using DevsCraft.Core.Objects;
+using NHibernate;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevsCraft.Core
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly ISession _session;
+        public BlogRepository(ISession session)
+        {
+            _session = session;
+        }
+        public IList<Post> Posts(int pageNo, int pageSize)
+        {
+            var posts = _session.Query<Post>().Where(p => p.Published).OrderByDescending(p => p.PostedOn).Skip(pageNo * pageSize).Take(pageSize).Fetch(p => p.Category).ToList();
+            var postId = posts.Select(p => p.Id).ToList();
+            return _session.Query<Post>()
+                .Where(p => postId.Contains(p.Id))
+                .OrderByDescending(p => p.PostedOn)
+                .FetchMany(p => p.Tags).ToList();
+        }
+        public int TotalPosts()
+        {
+            return _session.Query<Post>().Where(p => p.Published).Count();
+        }
+    }
+}
---
+++ DevsCraft.Core/IBlogRepository.cs
@@ -0,0 +1,15 @@
+﻿using DevsCraft.Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevsCraft.Core
+{
+    public interface IBlogRepository
+    {
+        IList<Post> Posts(int pageNo, int pageSize);
+        int TotalPosts();
+    }
+}
