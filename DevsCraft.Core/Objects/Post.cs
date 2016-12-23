c59b8ae9746ce155d51a3398d9d5c3b9c37ded2f
Moinul Islam <moinulmithu@hotmail.com>
2016-12-23 12:41:15 +0600

    2 new class added

--- DevsCraft.Core/Objects/Post.cs
+++ DevsCraft.Core/Objects/Post.cs
@@ -8,14 +8,16 @@ namespace DevsCraft.Core.Objects
 {
     class Post
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string ShortDescription { get; set; }
-        public string Description { get; set; }
-        public string UrlSlug { get; set; }
-        public string Published { get; set; }
-        public DateTime PostedOn { get; set; }
-        public DateTime Modified { get; set; }
-        public string Category { get; set; }
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string ShortDescription { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Meta { get; set; }
+        public virtual string UrlSlug { get; set; }
+        public virtual bool Published { get; set; }
+        public virtual DateTime PostedOn { get; set; }
+        public virtual DateTime? Modified { get; set; }
+        public virtual string Category { get; set; }
+        public virtual IList<Tag> Tags { get; set; }
     }
 }
---
+++ DevsCraft.Core/Objects/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevsCraft.Core.Objects
+{
+    class Tag
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string UrlSlag { get; set; }
+        public virtual string Description { get; set; }
+        public virtual IList<Post> Posts { get; set; }
+    }
+}
